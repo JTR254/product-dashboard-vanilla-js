73aca151818f9d5127a5d6443aea9efbb103561e
JTR254 <jackrindge@gmail.com>
2025-03-30 21:13:14 -0400

    Task 4 - Display products in the DOM with name, price, and image

--- cc_16.js
+++ cc_16.js
@@ -13,22 +13,41 @@ function fetchProductsThen() { // creates function that fetches data details
 
 // Task 3 - Add fetchProductsAsync using async/await and try/catch
 
-async function fetchProductsAsync() {
+async function fetchProductsAsync() { // creates function that waits for one action to be completed before proceeding
     try {
         const response = await fetch (BASE_URL)
         const products = await response.json()
-        displayProducts(products)
+        displayProducts(products);
         } catch (error) {
         handleError(error);
         }};
 
+// Task 4 - Display products in the DOM with name, price, and image
 function displayProducts(products) {
-    console.log('Displaying products:', products)
+    const container = document.querySelector('#product-container'); // selects the product-container
+    container.innerHTML = ''; // makes sure the inner HTML is cleared
+
+    products.slice(0, 5).forEach(product => { // chooses the first 5 products
+        const productDiv = document.createElement('div'); // creates a div element
+        const img = document.createElement('img'); // creates an img element
+    img.src = product.fields.image[0].url;
+    const name = document.createElement('h3'); // creates a header element
+    name.textContent = `${product.fields.name}`;
+    const price = document.createElement('p'); // creates a paragraph element
+    price.textContent = `$${(product.fields.price / 100).toFixed(2)}`; // displays the price and rounds it to the second decimal place
+    
+    productDiv.appendChild(img) // appends child element to parent element
+    productDiv.appendChild(name); // appends child element to parent element
+    productDiv.appendChild(price); // appends child element to parent element
+    container.appendChild(productDiv); // appends child element to parent element
+});
 };
 
 function handleError(error) {
     console.log('Error fetching products:', error)
 };
+
+
 
 
 
